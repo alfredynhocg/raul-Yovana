037cf9b80bca68bd82f9abbd3c5edf51353bb3b7
alfredynhocg <alfredynho@gmail.com>
2026-04-13 09:15:03 -0400

    Actualizar titulo y meta tags: Raul & Yovana

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -7,30 +7,30 @@ import '@assets/css/icomoon.css';
 import '@assets/css/bootstrap.css';
 import '@assets/css/style.css';
 
-const IMAGE_URL = `https://thekusuma.com/static/slide-6-4715e29302dbaa2ba21494c6258298d4.jpg`;
-const META_DESCRIPTION = `Dengan memohon Rahmat dan Ridho Illahi, teriring niat menjalankan Sunnah Rasulullah ﷺ untuk membentuk rumah tangga yang Sakinah, Mawaddah wa Rahmah, kami mohon do'a agar senantiasa diberikan kelancaran dan keberkahan. - Dinda & Indra`;
+const IMAGE_URL = `https://alfredynhocg.github.io/raul-Yovana/`;
+const META_DESCRIPTION = `Con la bendición de Dios, Raul y Yovana se unen en sagrado matrimonio cristiano el 9 de Mayo de 2026 en El Alto, Bolivia.`;
 
 function MainLayout({ children }) {
   return (
     <Fragment>
       <Helmet>
-        <title>Dinda ❤️ Indra Wedding</title>
+        <title>Raul ❤️ Yovana</title>
 
         {/* Favicon */}
         <link rel="icon" type="image/png" href={Favicon} />
 
         {/* font and SEO tags */}
-        <meta property="og:title" content="The Wedding of Dinda & Indra" />
+        <meta property="og:title" content="La Boda de Raul & Yovana" />
         <meta property="og:image" content={IMAGE_URL} />
-        <meta property="og:url" content="https://thekusuma.com" />
-        <meta property="og:site_name" content="The Wedding of Dinda & Indra" />
+        <meta property="og:url" content="https://alfredynhocg.github.io/raul-Yovana/" />
+        <meta property="og:site_name" content="La Boda de Raul & Yovana" />
         <meta property="og:description" content={META_DESCRIPTION} />
-        <meta name="twitter:title" content="The Wedding of Dinda & Indra" />
+        <meta name="twitter:title" content="La Boda de Raul & Yovana" />
         <meta name="twitter:description" content={META_DESCRIPTION} />
         <meta name="twitter:image" content={IMAGE_URL} />
-        <meta name="twitter:url" content="https://thekusuma.com" />
+        <meta name="twitter:url" content="https://alfredynhocg.github.io/raul-Yovana/" />
         <meta name="twitter:card" content="summary" />
-        <meta name="twitter:creator" content="@idindrakusuma" />
+        <meta name="twitter:creator" content="@alfredynhocg" />
 
         <link
           href="https://fonts.googleapis.com/css?family=Work+Sans:400,300,600,400italic,700"
